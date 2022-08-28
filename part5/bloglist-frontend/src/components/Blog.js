0d5d99bef204f8d9bdb59f0b8f438e36025079f7
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2022-08-28 21:54:24 +0300

    user can view or hide blog details, ex 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const [buttonLabel, setButtonLabel] = useState('view')
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const changeVisibility = () => {
+    setVisible(!visible)
+    buttonLabel === 'view' ? setButtonLabel('hide') : setButtonLabel('view')
+  }
+
+  // console.log('BLOG:', blog)
+
+  return (
+    <>
+      <div>
+        <br></br>
+        {blog.title} {blog.author}
+        <button onClick={changeVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <div> likes: 
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </>
+    )
+}
 
 export default Blog
