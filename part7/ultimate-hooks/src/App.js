f7e7dcc5e0edd47c6c39d0fd56bc61a06b0c9e51
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2022-09-28 14:13:26 +0300

    custom hook useResource can fetch all resources and create new, ex 7.8

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,22 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    console.log('response data', response.data)
+    setResources(response.data)
+  }
 
-  const create = (resource) => {
-    // ...
+  useEffect( () => {
+    getAll()
+  }, [])
+
+  const create = async (resource) => {
+    console.log('url', baseUrl)
+    const response = await axios.post(baseUrl, resource)
+    console.log('response data', response.data)
+    // setResources(res.data)
+    setResources([...resources, response.data])
   }
 
   const service = {
@@ -43,11 +55,13 @@ const App = () => {
 
   const handleNoteSubmit = (event) => {
     event.preventDefault()
+    console.log('handleNoteSubmit')
     noteService.create({ content: content.value })
   }
  
   const handlePersonSubmit = (event) => {
     event.preventDefault()
+    console.log('handlePersonSubmit')
     personService.create({ name: name.value, number: number.value})
   }
 
@@ -56,7 +70,7 @@ const App = () => {
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
         <input {...content} />
-        <button>create</button>
+        <button type='submit'>create</button>
       </form>
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
@@ -64,7 +78,7 @@ const App = () => {
       <form onSubmit={handlePersonSubmit}>
         name <input {...name} /> <br/>
         number <input {...number} />
-        <button>create</button>
+        <button type='submit'>create</button>
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
     </div>
